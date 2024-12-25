6ab944f5e85e28a60c6521cf15f6e2dbb208fbfa
DESKTOP-NPNP3FO\Asus TUF Gaming <daenit091@gmail.com>
2024-12-25 22:20:19 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Home } from './modules/Home';
+import { Routes, Route } from 'react-router-dom';
+import {ProductDetail} from './components/productDetail'
+import {Shop} from './modules/Shop'
+import { Cart } from './modules/Cart';
+import { Chechout } from './modules/Chechout';
+function App() {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/products" element={<Shop />} />
+        <Route path="cart" element={<Cart/>} />
+        <Route path="/checkout" element={<Chechout />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
